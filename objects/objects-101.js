e4a1b82375618a33ef9c0ca446b6cba8b374fe59
Hardik Kardam <69447549+LazyCROW21@users.noreply.github.com>
2022-05-18 01:24:36 +0530

    Create objects-101.js

---
+++ objects/objects-101.js
@@ -0,0 +1,20 @@
+let myBook = {
+    title: 'Harry Poter',
+    author: 'AASD ASD',
+    pages: 1020
+};
+
+console.log(myBook);
+console.log(myBook.title);
+
+myBook.pages = 1023;
+
+let person = {
+    name: 'Hardik',
+    age: 21,
+    location: 'Mehsana, India'
+}
+
+person.age = person.age + 1;
+let str = `I am ${person.name}, I am ${person.age} years old`;
+console.log(str);
