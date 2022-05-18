0046e817469855f4f1ab49636810ff4e6bbbabe5
LazyCROW21 <sasukeuchiha51047@gmail.com>
2022-05-18 18:27:58 +0530

    array almost done

--- arrays/notes.js
+++ arrays/notes.js
@@ -1,16 +1,67 @@
 
-const notes = ['Note 1', 'Note 2', 'Note 3'];
+const notes = [
+    {
+        title: 'Note 1',
+        body: 'Some big sentence with absolutely no meaning'
+    },
+    {
+        title: 'Note 2',
+        body: 'Some other big sentence with absolutely no meaning'
+    },
+    {
+        title: 'Note 3',
+        body: 'Some weird big sentence with absolutely no meaning'
+    }
+];
 
 // console.log(notes.pop());
 
 // notes.push('Note Push');
 // notes.unshift('Added To First');
 
-notes.splice(1, 0, 'Middle');
+// notes.splice(1, 0, 'Middle');
+
+// callback function
+// notes.forEach(function(element, idx){
+//     console.log(`Item ${idx + 1}:  ${element}`);
+// });
+
+// console.log(notes);
+// console.log(notes.length);
+// console.log(notes[1]);
+// console.log(notes.shift());
+
+// for (let i = 0; i < notes.length; i++) {
+//     console.log(i+1, notes[i]);
+// }
+
+// console.log(notes.indexOf({}));
+
+// let idx = notes.findIndex(function(note, idx){
+//     return note.title === 'Note 1';
+// });
+
+// this warapper can be replaced by Array.find()
+// let findNote = function(notes, noteTitle) {
+//     let idx = notes.findIndex(function(note){
+//         return note.title.toLowerCase() === noteTitle.toLowerCase();
+//     });
+
+//     if(idx === -1) {
+//         return {}
+//     }
+//     return notes[idx];
+// }
 
 
-console.log(notes);
-console.log(notes.length);
-console.log(notes[1]);
-console.log(notes.shift());
+const findNotes = function(notes, query) {
+    return notes.filter(function(note, idx) {
+        const st = query.toLowerCase();
+        const tm = note.title.toLowerCase().includes(st);
+        const bm = note.body.toLowerCase().includes(st);
+        return (tm || bm);
+    });
+}
 
+// console.log(findNote(notes, 'Note 2'));
+console.log(findNotes(notes, 'wei'));
--- arrays/todo.js
+++ arrays/todo.js
@@ -1,15 +1,51 @@
 const todos = [
-    'Play Valorant',
-    'Exercise',
-    'Eat',
-    'Hangout with Friends',
-    'Sleep'
+    {
+        title: 'Play Valorant',
+        completed: false
+    },
+    {
+        title: 'Sleep',
+        completed: false
+    },
+    {
+        title: 'Hangout with Friends',
+        completed: true
+    },
+    {
+        title: 'Eat',
+        completed: true
+    },
+    {
+        title: 'Exercise',
+        completed: false
+    }
 ];
 
-console.log(`You have ${todos.length} todos`);
-console.log(`First todo: ${todos[0]}`);
-console.log(`Last todo: ${todos[todos.length - 1]}`);
+// console.log(`You have ${todos.length} todos`);
+// console.log(`First todo: ${todos[0]}`);
+// console.log(`Last todo: ${todos[todos.length - 1]}`);
 
-todos.splice(2, 1);
-todos.push('Learn JS');
-todos.shift();
+// todos.splice(2, 1);
+// todos.push('Learn JS');
+// todos.shift();
+
+let deleteTodo = function(todos, title) {
+    let idx = todos.findIndex(function(todo){
+        return todo.title.toLowerCase() === title.toLowerCase();
+    });
+
+    if(idx !== -1) {
+        todos.splice(idx, 1);
+    }
+};
+
+const getPendingTodos = function(todos) {
+    return todos.filter(function(todo, idx) {
+        return todo.completed === false;
+    });
+}
+
+let pendingTodos = getPendingTodos(todos, 'Sleep');
+for (let i = 0; i < pendingTodos.length; i++) {
+    console.log(i+1, pendingTodos[i]);
+}
